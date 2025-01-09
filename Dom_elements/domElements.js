36444f01c41ac7a0f04daf3063ccdff678362e54
samuses <samuelemurru60@gmail.com>
2025-01-09 16:55:16 +0100

    dom elements exercise

---
+++ Dom_elements/domElements.js
@@ -0,0 +1,11 @@
+// Cerca tutti gli elementi con classe "paragrafo"
+const paragrafi = document.querySelectorAll('.paragrafo');
+console.log(paragrafi); // Stampa una NodeList con tutti gli elementi con classe "paragrafo"
+
+// Cerca l'elemento con id "elemento-3"
+const elemento3 = document.getElementById('elemento-3');
+console.log(elemento3); // Stampa l'elemento con id "elemento-3"
+
+// Cerca tutti gli elementi <h2> della pagina
+const titoliH2 = document.querySelectorAll('h2');
+console.log(titoliH2); // Stampa una NodeList con tutti gli elementi <h2>
