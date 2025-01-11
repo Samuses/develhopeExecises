51c66a187ffcef11a22f06f2be5daaf308ee071a
samuses <samuelemurru60@gmail.com>
2025-01-11 15:46:32 +0100

    list exercise

---
+++ reactEx13/List/src/Color.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Color({ color }) {
+  return <li>{color.name}</li>;
+}
+
+export default Color;
---
+++ reactEx13/List/src/Colors.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Color from "./Color";
+
+function Colors({ colors }) {
+  return (
+    <ul>
+      {colors.map((color) => (
+        <Color key={color.id} color={color} />
+      ))}
+    </ul>
+  );
+}
+
+export default Colors;
